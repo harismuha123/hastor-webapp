c770c009e589bcc2c2fd05f3f5cd1df7ef566ad2
harismuha123 <harismuha123@gmail.com>
2018-05-26 19:48:32 +0200

    implemented entire Reservations page, both backend and frontend, created routes with PersistenceManager functions which handle the data and return it from the database using PDO, created dynamic loading of data and buttons for creating, editing and deleting reservations.

--- rest/PersistenceManager.class.php
+++ rest/PersistenceManager.class.php
@@ -26,12 +26,15 @@ class PersistenceManager {
         $query = "SELECT stu.id, stu.name, surname, c.city AS city, address, email, tel1, gpa, school.name AS facility_name, year_of_study 
                   FROM SchoolStudents AS stu 
                   INNER JOIN Cities AS c ON stu.city_id = c.id
-                  INNER JOIN Schools AS school ON stu.school_id=school.id";
+                  INNER JOIN Schools AS school ON stu.school_id=school.id AND school.is_highschool = 0";
         return $this->pdo->query($query)->fetchAll();
     }
 
     public function get_all_highschool_students(){
-        $query = "SELECT * FROM SchoolStudents AND Schools";
+        $query = "SELECT stu.id, stu.name, surname, c.city AS city, address, email, tel1, gpa, school.name AS facility_name, year_of_study 
+                  FROM SchoolStudents AS stu 
+                  INNER JOIN Cities AS c ON stu.city_id = c.id
+                  INNER JOIN Schools AS school ON stu.school_id=school.id AND school.is_highschool = 1";
         return $this->pdo->query($query)->fetchAll();
     }
 
@@ -44,7 +47,7 @@ class PersistenceManager {
     }
 
     public function get_new_reservations() {
-        $query = "SELECT res.id AS reservation_id, time_of_reservation, for_date, from_hour, to_hour, res.address AS address, sidenote, is_accepted, c.city AS city, 
+        $query = "SELECT res.id AS reservation_id, time_of_reservation, for_date, from_hour, to_hour, res.address AS address, sidenote, is_accepted, c.city AS city, c.id AS city_id,
                   men.id AS mentor_id, stu.name AS name, stu.surname AS surname 
                   FROM Reservations AS res INNER JOIN Cities AS c ON res.city_id = c.id 
                   INNER JOIN Mentors AS men ON men.reservation_id = res.id 
@@ -53,13 +56,24 @@ class PersistenceManager {
         return $this->pdo->query($query)->fetchAll();
     }
 
+
     public function get_returned_reservations() {
-        $query = "SELECT res.id AS reservation_id, time_of_reservation, for_date, from_hour, to_hour, res.address AS address, sidenote, is_accepted, c.city AS city, 
+        $query = "SELECT res.id AS reservation_id, time_of_reservation, for_date, from_hour, to_hour, res.address AS address, sidenote, is_accepted, c.city AS city, c.id AS city_id,
                   men.id AS mentor_id, stu.name AS name, stu.surname AS surname 
                   FROM Reservations AS res INNER JOIN Cities AS c ON res.city_id = c.id 
                   INNER JOIN Mentors AS men ON men.reservation_id = res.id 
                   INNER JOIN UniversityStudents AS stu ON stu.id = men.student_id
                   WHERE res.is_accepted = 0";
+        return $this->pdo->query($query)->fetchAll();
+    }
+
+    public function get_accepted_reservations() {
+        $query = "SELECT res.id AS reservation_id, time_of_reservation, for_date, from_hour, to_hour, res.address AS address, sidenote, is_accepted, c.city AS city, c.id AS city_id,
+                  men.id AS mentor_id, stu.name AS name, stu.surname AS surname 
+                  FROM Reservations AS res INNER JOIN Cities AS c ON res.city_id = c.id 
+                  INNER JOIN Mentors AS men ON men.reservation_id = res.id 
+                  INNER JOIN UniversityStudents AS stu ON stu.id = men.student_id
+                  WHERE res.is_accepted = 1";
         return $this->pdo->query($query)->fetchAll();
     }
 
@@ -70,8 +84,46 @@ class PersistenceManager {
     }
 
     public function get_all_cities() {
-        $query = "SELECT city FROM Cities";
+        $query = "SELECT id, city FROM Cities";
         return $this->pdo->query($query)->fetchAll();
+    }
+
+
+    public function create_reservation($input, $student_id) {
+        $query = "INSERT INTO Reservations(time_of_reservation, for_date, from_hour, to_hour, address, sidenote, city_id)
+                  VALUES(NOW(), :for_date, :from_hour, :to_hour, :address, :sidenote, :city_id)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($input);
+        $reservation_id = $this->pdo->lastInsertId();
+        $newArray = array(
+            "student_id" => $student_id,
+            "reservation_id" => $reservation_id
+        );
+        return $newArray;
+    }
+
+    public function add_mentor($input) {
+        $query = "INSERT INTO Mentors(reservation_id, student_id)
+                  VALUES(:reservation_id, :student_id)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($input);
+    }
+
+    public function delete_reservation($id) {
+        $input = array();
+        array_push($input, $id, $id);
+        $query = "DELETE FROM Mentors WHERE reservation_id = ?;
+                  DELETE FROM Reservations WHERE id = ?";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($input);
+    }
+
+    public function check_reservation($input) {
+        $query = "UPDATE Reservations
+                  SET city_id = :city_id, address = :address, for_date = :for_date, from_hour = :from_hour, to_hour = :to_hour, sidenote = :sidenote, is_accepted = :is_accepted
+                  WHERE id = :reservation_id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($input);
     }
 }
 
--- rest/index.php
+++ rest/index.php
@@ -23,6 +23,16 @@ Flight::route('GET /studenti', function (){
    Flight::json(array( "students" => $uni_students ));
 });
 
+Flight::route('GET /volonteri', function (){
+    $volunteers = Flight::pm()->get_all_university_students();
+    Flight::json($volunteers);
+});
+
+Flight::route('GET /srednjoskolci', function (){
+    $school_students = Flight::pm()->get_all_highschool_students();
+    Flight::json(array("students" => $school_students));
+});
+
 Flight::route('GET /osnovci', function (){
     $school_students = Flight::pm()->get_all_school_students();
     Flight::json(array("students" => $school_students));
@@ -33,19 +43,80 @@ Flight::route('GET /pristigle', function (){
     Flight::json(array("reservations" => $new_reservations));
 });
 
+Flight::route('GET /pristigla_najava', function (){
+    $new_reservations = Flight::pm()->get_new_reservations();
+    Flight::json($new_reservations);
+});
+
 Flight::route('GET /vracene', function (){
     $returned_reservations = Flight::pm()->get_returned_reservations();
     Flight::json(array("reservations" => $returned_reservations));
 });
 
-Flight::route('GET /prihvacene', function (){
+Flight::route('GET /vracena_najava', function (){
     $returned_reservations = Flight::pm()->get_returned_reservations();
-    Flight::json(array("reservations" => $returned_reservations));
+    Flight::json($returned_reservations);
+});
+
+Flight::route('GET /prihvacene', function (){
+    $accepted_reservations = Flight::pm()->get_accepted_reservations();
+    Flight::json(array("reservations" => $accepted_reservations));
+});
+
+Flight::route('GET /prihvacena_najava', function (){
+    $accepted_reservations = Flight::pm()->get_accepted_reservations();
+    Flight::json($accepted_reservations);
 });
 
 Flight::route('GET /gradovi', function (){
     $cities = Flight::pm()->get_all_cities();
     Flight::json($cities);
+});
+
+Flight::route('POST /kreiraj_najavu', function () {
+    $student = Flight::request()->data->volunteer;
+    $city = Flight::request()->data->city;
+    $request = Flight::request();
+    $studentArray = explode(" ", $student);
+    $cityArray = explode(" ", $city);
+    $input = array(
+        "city_id" => $cityArray[0],
+        "address" => $request->data->address,
+        "for_date" => $request->data->for_date,
+        "from_hour" => $request->data->from_hour,
+        "to_hour" => $request->data->to_hour,
+        "sidenote" => $request->data->sidenote
+    );
+    $dataArray = Flight::pm()->create_reservation($input, $studentArray[0]);
+    Flight::pm()->add_mentor($dataArray);
+});
+
+Flight::route('DELETE /obrisi_najavu/@id', function($id){
+    Flight::pm()->delete_reservation($id);
+});
+
+Flight::route('POST /pregledaj_najavu/', function(){
+    $student = Flight::request()->data->volunteer;
+    $city = Flight::request()->data->city;
+    $acceptedString = Flight::request()->data->is_accepted;
+    $is_accepted = 0;
+    if ($acceptedString === "Da") {
+        $is_accepted = 1;
+    }
+    $request = Flight::request();
+    $studentArray = explode(" ", $student);
+    $cityArray = explode(" ", $city);
+    $input = array(
+        "city_id" => $cityArray[0],
+        "address" => $request->data->address,
+        "for_date" => $request->data->for_date,
+        "from_hour" => $request->data->from_hour,
+        "to_hour" => $request->data->to_hour,
+        "sidenote" => $request->data->sidenote,
+        "is_accepted" => $is_accepted,
+        "reservation_id" => $studentArray[0]
+    );
+    Flight::pm()->check_reservation($input);
 });
 
 Flight::route('DELETE /university_students/@id', function($id){
